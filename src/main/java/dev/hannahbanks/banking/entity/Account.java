7c4dabd7b60332e764bd10d89aa5fbf80f9a8474
Hannah Catherine Trask <hctrask@gmail.com>
2024-04-23 16:04:38 -0600

    Adds accounts table and some packages

---
+++ src/main/java/dev/hannahbanks/banking/entity/Account.java
@@ -0,0 +1,22 @@
+package dev.hannahbanks.banking.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "accounts")
+@Entity
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "account_holder_name")
+    private String accountHolderName;
+    private Double balance;
+}
