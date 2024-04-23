034a95a54924802236e1a30d4d1edfb7460c7860
Hannah Catherine Trask <hctrask@gmail.com>
2024-04-23 16:30:18 -0600

    Adds addAccount POST rest qpi req

---
+++ src/main/java/dev/hannahbanks/banking/controller/AccountController.java
@@ -0,0 +1,27 @@
+package dev.hannahbanks.banking.controller;
+
+import dev.hannahbanks.banking.dto.AccountDto;
+import dev.hannahbanks.banking.service.AccountService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/accounts")
+public class AccountController {
+
+    private AccountService accountService;
+
+    public AccountController(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    // add account REST API
+    @PostMapping
+    public ResponseEntity<AccountDto> addAccount(@RequestBody AccountDto accountDto) {
+        return new ResponseEntity<>(accountService.createAccount(accountDto), HttpStatus.CREATED);
+    }
+}
