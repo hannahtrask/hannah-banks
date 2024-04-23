bd2ad58e172c8747e5f0fc65209b86d45b40d2f1
Hannah Catherine Trask <hctrask@gmail.com>
2024-04-23 15:58:08 -0600

    Initial install and run of springboot app

---
+++ src/main/java/dev/hannahbanks/banking/HannahBankingAppApplication.java
@@ -0,0 +1,13 @@
+package dev.hannahbanks.banking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HannahBankingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HannahBankingAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/hannahbanks/banking/HannahBankingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.hannahbanks.banking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HannahBankingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
